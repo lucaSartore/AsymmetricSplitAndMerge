09f876230b1ff28941f035b126dc759824f9020c
lucaSartore <lucasar.com8@gmail.com>
2024-12-19 21:59:05 +0100

    Add split trait definition

--- src/splitter_trait/mod.rs
+++ src/splitter_trait/mod.rs
@@ -1,3 +1,6 @@
+use opencv::core::MatTrait;
+use crate::image_container::CutDirection;
 /// trait that can represent different splitting strategies
 pub trait SplitterTrait{
+    fn split(image: &impl MatTrait) -> Option<(CutDirection, i32)>;
 }
