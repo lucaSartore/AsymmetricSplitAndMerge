343a3d9b99879c7e038efbd63d0c1c721ace616b
lucaSartore <lucasar.com8@gmail.com>
2024-12-19 17:03:38 +0100

    Add logger trait template

---
+++ src/logger_trait/mod.rs
@@ -0,0 +1,5 @@
+/// trait used to log the behaviour of the split and merge algorithm, there can be different
+/// implementations including one that show the progress in rial time, one that save the result on
+/// a small video, an option that dose nothing and dose not impact performances eccetera
+pub trait LoggerTrait{
+}
