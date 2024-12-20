00e2cc00f3df3355316b34ee34913a177438366b
lucaSartore <lucasar.com8@gmail.com>
2024-12-20 13:48:25 +0100

    add blind splitter

---
+++ src/splitter_trait/blind_splitter.rs
@@ -0,0 +1,34 @@
+use crate::image_container::CutDirection;
+
+use super::SplitterTrait;
+
+
+/// BlindSplitter always decide to split up until a certain size
+pub struct BlindSplitter{
+    min_split_size: i32
+}
+impl BlindSplitter {
+    pub fn new(min_split_size: i32) -> Self{
+        assert!(min_split_size >= 2,"an image must be at least a 2x2 picture for split to take effect");
+        Self{min_split_size}
+    }
+}
+
+impl SplitterTrait for BlindSplitter {
+    fn split(&self, image: &impl opencv::prelude::MatTrait) -> Option<(crate::image_container::CutDirection, i32)> {
+        let size = image.size().expect("valid image must be passed");
+        if size.height > size.width{
+            // split the height
+            if size.height < self.min_split_size {
+                return None;
+            }
+            return Some((CutDirection::CutParallelToX, size.height/2));
+        }else{
+            // split the width
+            if size.width < self.min_split_size {
+                return None;
+            }
+            return Some((CutDirection::CutParallelToY, size.width/2));
+        }
+    } 
+}
--- src/splitter_trait/mod.rs
+++ src/splitter_trait/mod.rs
@@ -1,6 +1,9 @@
+mod blind_splitter;
+pub use blind_splitter::BlindSplitter;
+
 use opencv::core::MatTrait;
 use crate::image_container::CutDirection;
 /// trait that can represent different splitting strategies
-pub trait SplitterTrait{
-    fn split(image: &impl MatTrait) -> Option<(CutDirection, i32)>;
+pub trait SplitterTrait: Sync + 'static{
+    fn split(&self, image: &impl MatTrait) -> Option<(CutDirection, i32)>;
 }
